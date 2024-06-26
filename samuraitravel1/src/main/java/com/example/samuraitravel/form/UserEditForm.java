b744fa258b08e12a5007ec657bc2edb71c4c3441
MitsuiTaiga <166109271+MitsuiTaiga@users.noreply.github.com>
2024-06-26 17:30:32 +0900

    kadai_002

---
+++ samuraitravel1/src/main/java/com/example/samuraitravel/form/UserEditForm.java
@@ -0,0 +1,33 @@
+package com.example.samuraitravel.form;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+
+public class UserEditForm {
+	@NotNull
+	private Integer id;
+
+	@NotBlank(message = "氏名を入力して下さい。")
+	private String name;
+
+	@NotBlank(message = "ふりがなを入力してください。")
+	private String furigana;
+
+	@NotBlank(message = "郵便番号を入力してください。")
+	private String postalCode;
+
+	@NotBlank(message = "住所を入力してください。")
+	private String address;
+
+	@NotBlank(message = "電話番号を教えてください。")
+	private String phoneNumber;
+
+	@NotBlank(message = "メールアドレスを入力してください。")
+	private String email;
+
+}
