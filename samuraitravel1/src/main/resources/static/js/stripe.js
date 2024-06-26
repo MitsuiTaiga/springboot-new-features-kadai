c157d6d50e45a668242cf062d3a8277e0531313e
MitsuiTaiga <166109271+MitsuiTaiga@users.noreply.github.com>
2024-06-26 17:24:30 +0900

    Delete samuraitravel1 directory

--- samuraitravel1/src/main/resources/static/js/stripe.js
+++
@@ -1,8 +0,0 @@
- const stripe = Stripe('pk_test_51PQjYLRp0b525NJuc8JvrC52oCegMvcBieCeEpANsC07oOqDPlx8MNudwTh07ROUJV1BurWS1tcdYHryRggwHDor00xZt9mqeg');
- const paymentButton = document.querySelector('#paymentButton');
- 
- paymentButton.addEventListener('click', () => {
-   stripe.redirectToCheckout({
-     sessionId: sessionId
-   })
- });
